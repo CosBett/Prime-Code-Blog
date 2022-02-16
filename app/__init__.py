067fdb5de667db0a1d3b0def628c5957ad95e5f7
CosBett <cosmasbett9@gmail.com>
2022-02-16 10:50:41 +0300

    update signup and sign in forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import Config
+# from flask_bootstraps import Bootstrap
 
+# bootstrap = Bootstrap()
 app = Flask(__name__)
 
 def create_app():
@@ -12,6 +14,8 @@ def create_app():
 
   app.register_blueprint(main_blueprint)
   app.register_blueprint(auth_blueprint)
-
+  
+  # Initialise bootstrap instance
+  # bootstrap.init_app(app)
   
   return app
