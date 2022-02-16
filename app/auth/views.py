44dcbb41a2f2143e6842e86070bee14a249c0e0e
CosBett <cosmasbett9@gmail.com>
2022-02-16 14:42:25 +0300

    add route decorator for signing in

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,22 @@
-from flask import render_template, redirect,url_for
+from curses import flash
+from flask import render_template,request, redirect,url_for
+from flask_login import login_user,logout_user,login_required
 from app.auth import auth
+from app. models import User
+from .forms import SigninForm,SignupForm
 
-@auth.route('/signin')
+@auth.route('/signin', methods =['POST', 'GET'])
 def signin():
-  
-    return render_template('signin.html')
+    form = SigninForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Wrong Username or Password')
+
+    return render_template('signin.html',form = form)
+
 @auth.route('/signup')
 def signup():
   
