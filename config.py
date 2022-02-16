086f1bcf7de77990f6a435b708f53c45c684bb8f
CosBett <cosmasbett9@gmail.com>
2022-02-16 09:39:52 +0300

    update gitignore file

---
+++ config.py
@@ -0,0 +1,9 @@
+from distutils.debug import DEBUG
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Confiq:
+  DEBUG = True 
