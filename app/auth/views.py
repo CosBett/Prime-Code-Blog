4f62b851ddf85cfc2b15ff69e0dc76a0ceb66f5e
CosBett <cosmasbett9@gmail.com>
2022-02-16 14:45:57 +0300

    create flask welcome mail

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user,logout_user,login_required
 from app.auth import auth
 from app. models import User
 from .forms import SigninForm,SignupForm
-
+from ..email import mail_message
 @auth.route('/signin', methods =['POST', 'GET'])
 def signin():
     form = SigninForm()
@@ -19,5 +19,13 @@ def signin():
 
 @auth.route('/signup')
 def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email = form.email.data, password=form.password.data)
+        if user != None and user.verify_password(form.password.data):
+           user.save()
+           mail_message("Welcome to D-Blog","email/welcome",user.email,user=user)
+        return  redirect(url_for('auth.login'))
+    return render_template('auth/signup.html',registration_form=form )
   
     return render_template('signup.html')    
