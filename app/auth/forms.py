190194da6a0e319a93d1caa9e6ca134a5d9e5819
CosBett <cosmasbett9@gmail.com>
2022-02-16 11:15:32 +0300

    create user class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField, IntegerField,SubmitField,ValidationError
 from wtforms.validators import InputRequired,Length,Email
+from ..models import User
 
 class SigninForm(FlaskForm):
   username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -18,3 +19,10 @@ class SignupForm(FlaskForm):
   password_confirm =PasswordField('password', validators=[InputRequired(), Length(min=8, max=25)])
   submit = SubmitField('SignUp')
   
+  def validate_email(self,data_field):
+      if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email has already been taken!")
+
+  def validate_username(self, data_field):
+      if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username has already been taken")
