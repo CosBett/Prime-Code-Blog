b451d60eda367b2201fe681e2d977e76aec8b7f7
CosBett <cosmasbett9@gmail.com>
2022-02-17 04:00:19 +0300

    create test for blog class

---
+++ tests/blog_test.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models import Blog, User
+from app import db
+
+class BlogModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_charles = User(username='Mandellah', password='poi2134', email='mandellah94@gmail.com')
+        self.new_blog = Blog(id=1, title='Test', content='Blog to be tested', user_id=self.user_Mandellah.id)
+
+    def tearDown(self):
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_blog.title, 'Test')
+        self.assertEquals(self.new_blog.content, 'Blog to be tested')
+        self.assertEquals(self.new_blog.user_id, self.user_charles.id)
+
+    def test_save_blog(self):
+        self.new_blog.save()
+        self.assertTrue(len(Blog.query.all()) > 0)
+
+    def test_get_blog(self):
+        self.new_blog.save()
+        got_blog = Blog.get_blog(1)
+        self.assertTrue(got_blog is not None)
