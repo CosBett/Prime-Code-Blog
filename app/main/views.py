1aafed3c225b642d648500e740fb2bda81d86647
CosBett <cosmasbett9@gmail.com>
2022-02-17 05:48:42 +0300

    create profile and update profile views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
 from flask import render_template,redirect,url_for,abort,request,flash
 from app.main import main
+from .. import db
 from app.models import User,Blog,Comment,Subscriber
 from app.requests import get_quotes
+import secrets
+import os
+from PIL import Image
+from flask_login import login_required,current_user
+from .forms import UpdateProfile,CreateBlog
 
 @main.route('/')
 def index():
@@ -9,3 +15,48 @@ def index():
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/images', picture_filename)
+    
+    output_size = (200, 200)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_filename
+
+@main.route('/profile',methods = ['POST','GET'])
+@login_required
+def profile():
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.profile_pic_path = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.commit()
+        flash('Succesfully updated your profile')
+        return redirect(url_for('main.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    profile_pic_path = url_for('static',filename = 'photos/'+ current_user.profile_pic_path) 
+    return render_template('profile.html', profile_pic_path=profile_pic_path, form = form)    
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('updateprofile.html',form =form)
