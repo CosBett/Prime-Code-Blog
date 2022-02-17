ca87a88583b40d590f7c0b15c400f7f8d8ef04e5
CosBett <cosmasbett9@gmail.com>
2022-02-17 04:59:45 +0300

    create a get quote request for API

--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,11 @@
-from flask import request, json
+from email.quoprimime import quote
+from app import app
+import requests,json
 
-def get_quotes():
-    response = request.get('http://quotes.stormconsultancy.co.uk/random.json')
-    if response.status_code == 200:
-        quote = response.json()
-        return quote
+BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+def get_quotes(): 
+    response = requests.get(f"{BASE_URL}")
+    if response.status_code ==200:
+      quote = response.json()
+      return quote
+      
